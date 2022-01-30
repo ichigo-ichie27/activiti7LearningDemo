725fa546a5ecf0976ae92d920ce7c648883619b6
linchu <1090050869@qq.com>
2022-01-30 23:45:53 +0800

    新增会签demo

--- src/test/java/com/lc/test/TestCountersign.java
+++ src/test/java/com/lc/test/TestCountersign.java
@@ -1,33 +1,39 @@
 package com.lc.test;
-
 import org.activiti.engine.*;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.junit.Test;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-public class TestBpmn20 {
+/**
+ * 测试会签流程
+ */
+public class TestCountersign {
 
     /**
-     * 部署流程示例
+     * 部署流程定义
      */
     @Test
-    public void deploymentProcessInstance(){
-        // 1.创建processEngine
+    public void deploymentProcess(){
+        // 1.创建ProcessEngine
         ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
         // 2.获取RepositoryService
         RepositoryService repositoryService = processEngine.getRepositoryService();
-        // 3.使用repositoryService进行流程的部署
+        // 3.进行流程部署
         Deployment deploy = repositoryService.createDeployment()
-                .name("测试activiti是否支持bpmn20.xml")
-                .addClasspathResource("bpmn/test.bpmn20.xml")
+                .name("测试会签")
+                .addClasspathResource("bpmn/countersignDemo.bpmn20.xml")
                 .deploy();
         System.out.println("流程部署id："+deploy.getId());
         System.out.println("流程部署名称："+deploy.getName());
+
     }
 
     /**
-     * 启动流程示例
+     * 启动流程实例
      */
     @Test
     public void startProcessInstance(){
@@ -35,11 +41,12 @@ public class TestBpmn20 {
         ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
         // 2.获取RuntimeService
         RuntimeService runtimeService = processEngine.getRuntimeService();
-        // 3.根据流程定义的key启动流程示例
-        ProcessInstance instance = runtimeService.startProcessInstanceByKey("testBpmn");
-        System.out.println("流程定义id："+instance.getProcessDefinitionId());
-        System.out.println("流程示例id："+instance.getProcessInstanceId());
-
+        Map<String, Object> variables = new HashMap<String,Object>();
+        variables.put("approverList", Arrays.asList("xiaoliu","xiaozhou","xiaowang"));
+        // 3.根据流程定义的key启动流程实例
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("countersignDemo",variables);
+        System.out.println("流程定义id："+processInstance.getProcessDefinitionId());
+        System.out.println("流程示例id："+processInstance.getProcessInstanceId());
     }
 
     /**
@@ -49,16 +56,15 @@ public class TestBpmn20 {
     public void completeTask(){
         // 1.创建ProcessEngine
         ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
-        // 2.获取TaskService
+        // 2.获取RuntimeService
         TaskService taskService = processEngine.getTaskService();
         // 3.获取个人任务
-        Task task = taskService.createTaskQuery().processDefinitionKey("testBpmn")
-                .taskAssignee("yangbo").singleResult();
-        System.out.println("任务id="+task.getId());
-        System.out.println("任务负责人="+task.getAssignee());
-        System.out.println("任务名称="+task.getName());
-        // 4.通过任务id完成个人任务
-        taskService.complete(task.getId());
+        Task task = taskService.createTaskQuery().processDefinitionKey("countersignDemo")
+                .taskAssignee("wangwu").singleResult();
+        if(task != null){
+            // 4.通过任务id完成个人任务
+            taskService.complete(task.getId());
+        }
     }
 
 }
